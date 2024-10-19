35b8c1ee8fe26fe043585b671fd98f9d2dec575e
Octowl <alsaff1987@gmail.com>
2024-10-19 16:15:51 +0300

    move navbar to layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,5 @@
-import Navbar from "@/components/Navbar";
-import TripDetail from "@/components/TripDetail";
-import TripList from "@/components/TripList";
 import Welcome from "@/components/Welcome";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Welcome />
-    </div>
-  );
+  return <Welcome />;
 }
